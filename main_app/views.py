5f05629b86718dda11b37f0728a5b0acd1d84e4e
erictau <erictau04@gmail.com>
2022-07-15 14:27:37 -0700

    Updates login page and adds 'dummy' urls and view functions

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    pass
+
+def about(request):
+    pass
+
+def index(request):
+    pass
+
+def signup(request):
+    pass
