5d2d0759cab18241990826f1eba6f7aa8673949e
FrugalCarrot7 <james.devine1995@gmail.com>
2022-07-18 16:32:39 -0700

    adds dept det path

--- main_app/urls.py
+++ main_app/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('organizations/new', views.organizations_new, name='organizations_new'), 
     path('organizations/create', views.organizations_create, name='organizations_create'),
     path('departments/new', views.departments_new, name='departments_new'), 
-    path('departments/create', views.departments_create, name='departments_create'), 
+    path('departments/create', views.departments_create, name='departments_create'),
+    path('departments/<int:pk>/', views.departments_detail, name='departments_detail'),
     path('departments/<int:department_id>/tasks_create', views.tasks_create, name='tasks_create'),
     path('tasks/<int:pk>', views.TaskDetail.as_view(), name='tasks_detail'),
 ]
