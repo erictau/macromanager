9d36e17ced675617f014a223af8155361812819a
erictau <erictau04@gmail.com>
2022-07-18 09:25:21 -0700

    Adds org and department urls and views

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('index/', views.index, name='index'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('organizations/create/', views.OrganizationCreate.as_view(), name='organization_create'), # Eric: May need to change this to a custom view function.
+    path('departments/', views.DepartmentList.as_view(), name='department_index'), 
+    path('departments/<int:pk>', views.DepartmentDetail.as_view(), name='department_detail'),
+
 ]
